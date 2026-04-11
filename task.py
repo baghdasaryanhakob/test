d129b935fa6eb9e76e7b73119d9ec507da4e3573
baghdasaryanhakob <baghdasaryanhakob@gmail.com>
2026-04-11 20:20:34 +0400

    feat: add color percentage calculation script and update test implementation

--- task.py
+++ task.py
@@ -3,7 +3,6 @@ data = [
     [8, 32, 20, 5]
 ]
 color = input("Enter color: ")
-# colors are brown, blue, green, black
 
 brown = 0
 blue = 1
@@ -11,27 +10,28 @@ green = 2
 black = 3
 
 total = 0
+
 for i in range(len(data)):
     for j in range(len(data[i])):
         total += data[i][j]
 
 if color == "brown":
     brown_color = data[0][brown] + data[1][brown]
-    brown_share = int(round((brown_color / total) * 100, 0))
+    brown_share = int(round(brown_color / total * 100, 2))
     print(brown_share)
 
 if color == "blue":
     blue_color = data[0][blue] + data[1][blue]
-    blue_share = int(round((blue_color / total) * 100, 0))
+    blue_share = int(round(blue_color / total * 100, 2))
     print(blue_share)
 
 if color == "green":
     green_color = data[0][green] + data[1][green]
-    green_share = int(round((green_color / total) * 100, 0))
+    green_share = int(round(green_color / total * 100, 2))
     print(green_share)
 
 if color == "black":
     black_color = data[0][black] + data[1][black]
-    black_share = int(round((black_color / total) * 100, 0))
+    black_share = int(round(black_color / total * 100, 2))
     print(black_share)
 
